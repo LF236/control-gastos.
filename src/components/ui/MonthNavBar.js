8db32f64a7ccd7de0c4e0b9deb577e27741dd3b2
LF236 <fernandorodriguez236@gmail.com>
2023-07-11 07:57:20 -0600

    Navbar

--- src/components/ui/MonthNavBar.js
+++ src/components/ui/MonthNavBar.js
@@ -1,71 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect, createRef, useRef } from 'react';
 import '../../styles/components/mothNavBar.scss';
 const moths = [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre' ];
 const MonthNavBar = () => {
-    
+    const [ activeMoth, setActiveMonth ] = useState( 'Enero' );
+    const navContentRef = useRef( null );
+    let dragConent = false;
+
+    const handleChangeMoth = ( moth )  => {
+        setActiveMonth( moth );
+        console.log( navContentRef );
+    }
+
+    const drag = (e) => {
+        if (!dragConent) return;
+        console.log( navContentRef )
+        if( navContentRef.current ) navContentRef.current.classList.add( 'dragging' );
+        if( navContentRef.current ) navContentRef.current.scrollLeft -= e.movementX;
+    };
+
+    useEffect( () => {
+        if( navContentRef.current ) {
+            navContentRef.current.addEventListener( 'mousedown', () => {
+                console.log( 'Mouse down' );
+                dragConent = true;
+                console.log( dragConent );
+            });
+            
+            navContentRef.current.addEventListener( 'mousemove', drag);
+            
+            document.addEventListener( 'mouseup', () => {
+                console.log( 'MOUSE UP' )
+                dragConent = false;
+                if( navContentRef.current ) navContentRef.current.classList.remove( 'dragging');
+            });
+        }
+    }, [ navContentRef.current ] );
 
     return (
-        <div className='nav-content'>
-            <ul className='nav justify-content-center'>
+        <div className='nav-content' ref={ navContentRef }>
+            <ul className='nav' >
                 { moths.map( ele => (
-                    <li className="nav-item">
-                        <a className="nav-link active " href="#">{ ele }</a>
+                    <li className='nav-item'>
+                        <a 
+                            className={ `nav-link ${ ( ele === activeMoth )&& 'active' }` }
+                            href='#'
+                            onClick={ () => handleChangeMoth( ele ) }
+                        >
+                            { ele }
+                        </a>
                     </li>
                 ) ) }       
-
-                {/* <li className="nav-item especial">
-                    <a className="nav-link active" href="#">{ 'especial' }</a>
-                </li>          */}
-{/* 
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">1</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">2</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">3</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">4</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">5</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">6</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">7</a>
-                </li>
-
-                <li className="nav-item especial">
-                    <a className="nav-link active" href="#">8</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">9</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">10</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active" href="#">11</a>
-                </li>
-
-                <li className="nav-item">
-                    <a className="nav-link active " href="#">12</a>
-                </li> */}
-
-
             </ul>
         </div>
     );
